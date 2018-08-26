d35fa79bdef68aa0a42040b2f8db2138bacec9a4
rakuraku.musen <musen.rakuraku@gmail.com>
2018-08-26 09:46:54 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'toppages#index'
+  
+  get "signup" => "users#new"
+  
+  get "login" => "sessions#new"
+  post "login" => "sessions#create"
+  delete "logout" => "sessions#destroy"
+  
+  resources :users, only: [:index, :show, :new, :create]
 end
