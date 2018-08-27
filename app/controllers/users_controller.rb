64ed5b3761bc62b2659fce38ef0bf5bc2af72122
rakuraku.musen <musen.rakuraku@gmail.com>
2018-08-27 08:49:17 +0000

    post

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microopsts.order('created_at DESC').page(params[:page])
+    counts(@user)
   end
 
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,5 @@ Rails.application.routes.draw do
   delete "logout" => "sessions#destroy"
   
   resources :users, only: [:index, :show, :new, :create]
+  resources :microposts, only: [:create, :destroy]
 end
